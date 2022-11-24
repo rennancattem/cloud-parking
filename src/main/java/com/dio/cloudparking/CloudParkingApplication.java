7b27a4aeb4aa07dbcf73e0419b9fee25721f83ad
cattem <cattemsud@gmail.com>
2022-11-24 19:34:35 -0300

    Projeto desafio de Cloud Parking

--- src/main/java/com/dio/cloudparking/CloudParkingApplication.java
+++ src/main/java/com/dio/cloudparking/CloudParkingApplication.java
@@ -1,5 +1,6 @@
 package com.dio.cloudparking;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CloudParkingApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CloudParkingApplication.class, args);
 	}
 
--- src/main/java/com/dio/cloudparking/controller/HelloController.java
+++ src/main/java/com/dio/cloudparking/controller/HelloController.java
@@ -1,2 +1,15 @@
-package com.dio.cloudparking.controller;public class HelloController {
+package com.dio.cloudparking.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+    @GetMapping
+    public String hello(){
+        return "Hello Dio. Java Devs";
+    }
 }
