b2cc926a1fd12583a387d2273dfc11831706a65e
cattem <cattemsud@gmail.com>
2022-11-27 20:52:24 -0300

    Finalizando projeto

--- src/test/java/com/dio/cloudparking/controller/ParkingControllerTest.java
+++ src/test/java/com/dio/cloudparking/controller/ParkingControllerTest.java
@@ -24,6 +24,8 @@ class ParkingControllerTest extends AbstractContainerBase{
     @Test
     void whenFindAllThenCheckResult() {
         RestAssured.given()
+                .auth()
+                .basic("user", "Dio@123456")
                 .when()
                 .get("/parking")
                 .then()
@@ -42,6 +44,8 @@ class ParkingControllerTest extends AbstractContainerBase{
         createDTO.setState("AC");
 
         RestAssured.given()
+                .auth()
+                .basic("user", "Dio@123456")
                 .when()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(createDTO)
